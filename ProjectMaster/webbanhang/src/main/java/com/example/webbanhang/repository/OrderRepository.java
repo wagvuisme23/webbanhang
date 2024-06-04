9a98c999dcb397da7f71db7ebae873c04aed403e
wagvuisme23 <152259242+wagvuisme23@users.noreply.github.com>
2024-06-04 21:48:28 +0700

    Delete ProjectMaster/webbanhang directory

--- ProjectMaster/webbanhang/src/main/java/com/example/webbanhang/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.webbanhang.repository;
-
-import com.example.webbanhang.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
